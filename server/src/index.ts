cabc33da22e6de83b6a126bff506a6d4d9e9e908
lucastgama <lucas_tgama@hotmail.com>
2025-02-04 21:35:44 -0300

    feat: backend install

---
+++ server/src/index.ts
@@ -0,0 +1,30 @@
+import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+/* Route imports */
+
+/* Coniguratin */
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+/* Routes */
+app.get('/',(req,res)=>{
+    res.send('Hello World')
+})
+
+/* Server */
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
