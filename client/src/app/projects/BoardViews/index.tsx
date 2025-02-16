4a5cc0f5f92b5cfbf1353430de81246effee7ca5
lucastgama <lucas_tgama@hotmail.com>
2025-02-16 20:53:22 -0300

    fix: Image Un-configured Host

--- client/src/app/projects/BoardViews/index.tsx
+++ client/src/app/projects/BoardViews/index.tsx
@@ -176,7 +176,7 @@ const Task = ({ task }: TaskProps) => {
     >
       {task.attachments && task.attachments.length > 0 && (
         <Image
-          src=""
+          src="https://upload.wikimedia.org/wikipedia/commons/c/ce/Humpback_Whales_-_South_Bank.jpg"
           alt={task.attachments[0].fileName}
           width={400}
           height={200}
@@ -227,7 +227,7 @@ const Task = ({ task }: TaskProps) => {
             {task.assignee && (
               <Image
                 key={task.assignee.userId}
-                src=""
+                src="https://upload.wikimedia.org/wikipedia/commons/c/ce/Humpback_Whales_-_South_Bank.jpg"
                 alt={task.assignee.username}
                 width={30}
                 height={30}
@@ -237,7 +237,7 @@ const Task = ({ task }: TaskProps) => {
             {task.author && (
               <Image
                 key={task.author.userId}
-                src=""
+                src="https://upload.wikimedia.org/wikipedia/commons/c/ce/Humpback_Whales_-_South_Bank.jpg"
                 alt={task.author.username}
                 width={30}
                 height={30}
